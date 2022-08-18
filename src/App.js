b746a399222c611cca813dc000266b880fa46a6e
Guus van de Wal <info@guusvandewal.nl>
2022-08-18 15:51:52 +0200

    Added test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Albums from "./components/Albums";
 const App = () => {
   return (
     <div className="App">
+      <h1>Deezer playlist</h1>
       <Albums></Albums>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("Find h1", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Deezer playlist/i);
   expect(linkElement).toBeInTheDocument();
 });
